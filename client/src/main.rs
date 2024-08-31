65ee11bfb79ae06f9ab873f51b05a4906d400107
hayas1 <h4ystack@gmail.com>
2024-08-31 08:44:02 +0000

    server / client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let url = "http://localhost:3000";
+    let contents = reqwest::get(url).await?.text().await?;
+    println!("{:?}", contents);
+    Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    async fn root_handler() -> String {
+        "Hello World".to_string()
+    }
+
+    let app = axum::Router::new().route("/", get(root_handler));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    Ok(axum::serve(listener, app).await?)
 }
