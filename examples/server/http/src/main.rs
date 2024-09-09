3f1d6cb29cb6f9af8c72aa0310c61f6dfe2a3ad5
hayas1 <h4ystack@gmail.com>
2024-09-09 14:06:59 +0000

    graceful

--- examples/server/http/src/main.rs
+++ examples/server/http/src/main.rs
@@ -2,21 +2,26 @@ use axum::routing::get;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    #[tracing::instrument]
-    async fn root_handler() -> String {
-        "Hello World".to_string()
-    }
-
     tracing_subscriber::fmt::init();
+
     let app = axum::Router::new()
         .route("/", get(root_handler))
-        .layer(axum::middleware::from_fn(log_request_response));
+        .layer(axum::middleware::from_fn(logging));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
-    Ok(axum::serve(listener, app).await?)
+    let listening = "0.0.0.0:3000";
+    let listener = tokio::net::TcpListener::bind(listening).await?;
+    let serve = axum::serve(listener, app).with_graceful_shutdown(async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+        tracing::info!("stop app");
+    });
+
+    tracing::info!("start app on {}", listening);
+    Ok(serve.await?)
 }
 
-pub async fn log_request_response(
+pub async fn logging(
     req: axum::http::Request<axum::body::Body>,
     next: axum::middleware::Next,
     // ) -> Result<impl axum::response::IntoResponse, (axum::http::StatusCode, String)> {
@@ -26,4 +31,9 @@ pub async fn log_request_response(
     let status = res.status();
     tracing::info!("{} {} {}", status, method, uri);
     res
+}
+
+#[tracing::instrument]
+async fn root_handler() -> String {
+    "Hello World".to_string()
 }
