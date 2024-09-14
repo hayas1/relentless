eb74702beed7295c26cc99812ce3dd8419db4f94
hayas1 <h4ystack@gmail.com>
2024-09-14 01:02:40 +0000

    assault / compare

---
+++ examples/http_yaml_compare.rs
@@ -0,0 +1,8 @@
+use relentless::config::Config;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let (worker, testcases) = Config::read("./examples/compare.yaml")?.instance()?;
+    let result = worker.assault(testcases).await?;
+    Ok(result)
+}
