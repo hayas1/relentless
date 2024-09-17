d4fa8bce8f111cbe5b9834525d3caf62ed27f040
hayas1 <h4ystack@gmail.com>
2024-09-17 13:40:23 +0000

    path

--- examples/http_yaml_compare.rs
+++ examples/http_yaml_compare.rs
@@ -2,7 +2,7 @@ use relentless::Relentless;
 
 #[tokio::main]
 async fn main() -> Result<std::process::ExitCode, Box<dyn std::error::Error + Send + Sync>> {
-    let relentless = Relentless::read_paths(vec!["./examples/config/compare.yaml"])?;
+    let relentless = Relentless::read_paths(vec!["examples/config/compare.yaml"])?;
     let result = relentless.assault().await?;
     println!("{:#?}", result);
     Ok(result.exit_code(false))
