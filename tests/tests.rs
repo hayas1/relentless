fae0fb82842e3e60f119a4bc7b8c9f881445dc83
hayas1 <h4ystack@gmail.com>
2024-09-18 13:03:55 +0000

    test code

---
+++ tests/tests.rs
@@ -0,0 +1,25 @@
+use axum::{
+    body::{to_bytes, Body, Bytes, HttpBody},
+    http::{HeaderMap, Request, StatusCode},
+};
+use relentless::{
+    config::{Config, Testcase},
+    Relentless,
+};
+use serde::de::DeserializeOwned;
+use tower::ServiceExt;
+
+use example_http_server::{
+    route::{self, health::Health},
+    state::AppState,
+};
+
+#[tokio::test]
+async fn test_example_assault() -> Result<(), Box<dyn std::error::Error>> {
+    let (config, service) =
+        (Config::read("examples/config/assault.yaml")?, route::app(AppState { env: Default::default() }));
+    let relentless = Relentless::new(vec![config], Some(service));
+    let result = relentless.assault().await?;
+
+    Ok(())
+}
