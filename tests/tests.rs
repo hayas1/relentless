0762f780800ce009a73f046cf2056de1f7545f05
hayas1 <h4ystack@gmail.com>
2024-09-26 11:57:30 +0000

    no_report

--- tests/tests.rs
+++ tests/tests.rs
@@ -5,7 +5,8 @@ use example_http_server::{route, state::AppState};
 
 #[tokio::test]
 async fn test_example_assault() {
-    let relentless = Assault { file: vec!["examples/config/assault.yaml".into()], ..Default::default() };
+    let mut relentless = Assault { file: vec!["examples/config/assault.yaml".into()], ..Default::default() };
+    relentless.no_report = true; // TODO builder pattern ?
     let services = [("test-api".to_string(), route::app(AppState { env: Default::default() }))].into_iter().collect();
     let outcome = relentless.execute_with::<_, Body, Body>(vec![services]).await.unwrap();
 
