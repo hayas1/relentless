8f0ff70c381562505f7b915f95f23c5f6b1f652d
hayas1 <h4ystack@gmail.com>
2024-09-26 12:13:23 +0000

    remove TODO

--- tests/tests.rs
+++ tests/tests.rs
@@ -5,8 +5,8 @@ use example_http_server::{route, state::AppState};
 
 #[tokio::test]
 async fn test_example_assault() {
-    let mut relentless = Assault { file: vec!["examples/config/assault.yaml".into()], ..Default::default() };
-    relentless.no_report = true; // TODO builder pattern ?
+    let relentless =
+        Assault { file: vec!["examples/config/assault.yaml".into()], no_report: true, ..Default::default() };
     let services = [("test-api".to_string(), route::app(AppState { env: Default::default() }))].into_iter().collect();
     let outcome = relentless.execute_with::<_, Body, Body>(vec![services]).await.unwrap();
 
