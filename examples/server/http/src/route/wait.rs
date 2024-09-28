119c3cb5d9d753253e134d6f78ca428ec7f97f02
hayas1 <h4ystack@gmail.com>
2024-09-28 16:41:44 +0000

    unit type implementation

--- examples/server/http/src/route/wait.rs
+++ examples/server/http/src/route/wait.rs
@@ -1,4 +1,4 @@
-use std::time::Duration;
+use std::{fmt::Display, marker::PhantomData, time::Duration};
 
 use axum::{extract::Path, response::Result, routing::get, Json, Router};
 use serde::{Deserialize, Serialize};
@@ -8,29 +8,65 @@ use crate::state::AppState;
 
 pub fn route_wait() -> Router<AppState> {
     Router::new()
-        .route("/:duration", get(wait))
-        .route("/:duration/s", get(wait))
-        .route("/:duration/ms", get(wait_ms))
-        .route("/:duration/ns", get(wait_ns))
+        .route("/:duration", get(wait::<unit::Seconds>))
+        .route("/:duration/s", get(wait::<unit::Seconds>))
+        .route("/:duration/ms", get(wait::<unit::Milliseconds>))
+        .route("/:duration/ns", get(wait::<unit::Nanoseconds>))
+    // .fallback() // TODO
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub struct WaitResponse {}
+pub struct WaitResponse<U> {
+    pub duration: u64,
+    pub unit: PhantomData<U>,
+}
 
-#[tracing::instrument]
-pub async fn wait(Path(duration): Path<u64>) -> Result<Json<WaitResponse>> {
-    sleep(Duration::from_secs(duration)).await;
-    Ok(Json(WaitResponse {}))
+pub mod unit {
+    use super::*;
+
+    pub trait DurationUnit: Display {
+        fn duration(duration: u64) -> Duration;
+    }
+
+    pub enum Seconds {}
+    impl DurationUnit for Seconds {
+        fn duration(duration: u64) -> Duration {
+            Duration::from_secs(duration)
+        }
+    }
+    impl Display for Seconds {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "s")
+        }
+    }
+
+    pub enum Milliseconds {}
+    impl DurationUnit for Milliseconds {
+        fn duration(duration: u64) -> Duration {
+            Duration::from_millis(duration)
+        }
+    }
+    impl Display for Milliseconds {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "ms")
+        }
+    }
+
+    pub enum Nanoseconds {}
+    impl DurationUnit for Nanoseconds {
+        fn duration(duration: u64) -> Duration {
+            Duration::from_nanos(duration)
+        }
+    }
+    impl Display for Nanoseconds {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "ns")
+        }
+    }
 }
 
 #[tracing::instrument]
-pub async fn wait_ms(Path(duration): Path<u64>) -> Result<Json<WaitResponse>> {
-    sleep(Duration::from_millis(duration)).await;
-    Ok(Json(WaitResponse {}))
-}
-
-#[tracing::instrument]
-pub async fn wait_ns(Path(duration): Path<u64>) -> Result<Json<WaitResponse>> {
-    sleep(Duration::from_nanos(duration)).await;
-    Ok(Json(WaitResponse {}))
+pub async fn wait<U: unit::DurationUnit>(Path(duration): Path<u64>) -> Result<Json<WaitResponse<U>>> {
+    sleep(U::duration(duration)).await;
+    Ok(Json(WaitResponse { duration, unit: PhantomData }))
 }
