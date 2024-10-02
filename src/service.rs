46aa526231ea2515105f7688a3f9a8ff6171bcf2
hayas1 <h4ystack@gmail.com>
2024-10-02 09:53:15 +0000

    TODO

--- src/service.rs
+++ src/service.rs
@@ -23,6 +23,7 @@ pub struct DefaultHttpClient<ReqB, ResB> {
 }
 impl<ReqB, ResB> DefaultHttpClient<ReqB, ResB> {
     pub async fn new(_host: &str) -> RelentlessResult<Self> {
+        // TODO use hyper ? continue to use reqwest's rich client?
         let client = reqwest::Client::builder().user_agent(APP_USER_AGENT).build()?;
         Ok(Self { client, phantom: std::marker::PhantomData })
     }
