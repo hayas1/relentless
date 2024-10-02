25b228ed7f940a38295534b9905c1f5a14faa257
hayas1 <h4ystack@gmail.com>
2024-10-02 09:48:55 +0000

    dependencies

--- src/service.rs
+++ src/service.rs
@@ -14,7 +14,7 @@ use crate::{
     error::{RelentlessError, RelentlessResult},
 };
 
-static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
+const APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 
 #[derive(Debug)]
 pub struct DefaultHttpClient<ReqB, ResB> {
