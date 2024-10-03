ce77d9d3bc251c86158d2a20ff6fdd83ca22ec4e
hayas1 <h4ystack@gmail.com>
2024-10-03 13:35:04 +0000

    Destinations()

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,5 +1,5 @@
 use axum::body::Body;
-use relentless::command::Relentless;
+use relentless::{command::Relentless, config::Destinations};
 
 use example_http_server::route;
 
@@ -7,7 +7,7 @@ use example_http_server::route;
 async fn test_example_assault() {
     let relentless =
         Relentless { file: vec!["examples/config/assault.yaml".into()], no_report: true, ..Default::default() };
-    let services = [("test-api".to_string(), route::app_with(Default::default()))].into_iter().collect();
+    let services = Destinations([("test-api".to_string(), route::app_with(Default::default()))].into_iter().collect());
     let outcome = relentless.assault_with::<_, Body, Body>(vec![services]).await.unwrap();
 
     assert!(outcome.allow(false));
