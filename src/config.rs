ec2ed6cb7f5a14a7f1cc6f225c8aa1abf164f313
hayas1 <h4ystack@gmail.com>
2024-10-19 12:39:23 +0000

    _http

--- src/config.rs
+++ src/config.rs
@@ -297,6 +297,21 @@ mod _http {
     use std::ops::{Deref, DerefMut};
 
     use super::*;
+
+    #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+    pub struct Method(#[serde(with = "http_serde::method")] pub http::Method);
+    impl Deref for Method {
+        type Target = http::Method;
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
+    }
+    impl DerefMut for Method {
+        fn deref_mut(&mut self) -> &mut Self::Target {
+            &mut self.0
+        }
+    }
+
     #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
     pub struct StatusCode(#[serde(with = "http_serde::status_code")] pub http::StatusCode);
     impl Deref for StatusCode {
@@ -306,6 +321,62 @@ mod _http {
         }
     }
     impl DerefMut for StatusCode {
+        fn deref_mut(&mut self) -> &mut Self::Target {
+            &mut self.0
+        }
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+    pub struct Uri(#[serde(with = "http_serde::uri")] pub http::Uri);
+    impl Deref for Uri {
+        type Target = http::Uri;
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
+    }
+    impl DerefMut for Uri {
+        fn deref_mut(&mut self) -> &mut Self::Target {
+            &mut self.0
+        }
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+    pub struct Version(#[serde(with = "http_serde::version")] pub http::Version);
+    impl Deref for Version {
+        type Target = http::Version;
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
+    }
+    impl DerefMut for Version {
+        fn deref_mut(&mut self) -> &mut Self::Target {
+            &mut self.0
+        }
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+    pub struct HeaderMap(#[serde(with = "http_serde::header_map")] pub http::HeaderMap);
+    impl Deref for HeaderMap {
+        type Target = http::HeaderMap;
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
+    }
+    impl DerefMut for HeaderMap {
+        fn deref_mut(&mut self) -> &mut Self::Target {
+            &mut self.0
+        }
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+    pub struct Authority(#[serde(with = "http_serde::authority")] pub http::uri::Authority);
+    impl Deref for Authority {
+        type Target = http::uri::Authority;
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
+    }
+    impl DerefMut for Authority {
         fn deref_mut(&mut self) -> &mut Self::Target {
             &mut self.0
         }
