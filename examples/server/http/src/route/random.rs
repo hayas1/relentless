239c4352b689cb9db06ee36a28ae8d491de89894
hayas1 <h4ystack@gmail.com>
2024-10-22 16:43:13 +0000

    response

--- examples/server/http/src/route/random.rs
+++ examples/server/http/src/route/random.rs
@@ -1,5 +1,6 @@
 use axum::{response::Result, routing::get, Json, Router};
 use rand::distributions::{Alphanumeric, DistString};
+use serde::{Deserialize, Serialize};
 
 use crate::state::AppState;
 
@@ -9,6 +10,14 @@ pub fn route_random() -> Router<AppState> {
         .route("/int", get(randint))
         .route("/float", get(random))
         .route("/string", get(rands))
+        .route("/response", get(random_response))
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct RandomResponse {
+    pub int: i64,
+    pub float: f64,
+    pub string: String,
 }
 
 #[tracing::instrument]
@@ -26,4 +35,9 @@ pub async fn rands() -> Result<String> {
     let mut rng = rand::thread_rng();
     let sample = Alphanumeric.sample_string(&mut rng, 32);
     Ok(sample)
+}
+
+#[tracing::instrument]
+pub async fn random_response() -> Result<Json<RandomResponse>> {
+    Ok(Json(RandomResponse { int: randint().await?.0, float: random().await?.0, string: rands().await? }))
 }
