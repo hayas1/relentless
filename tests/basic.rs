4c8ca0197b37fc47e2651f391970416c325d950e
hayas1 <h4ystack@gmail.com>
2024-10-26 15:23:13 +0000

    yaml / toml config

--- tests/basic.rs
+++ tests/basic.rs
@@ -1,5 +1,3 @@
-#![cfg(all(feature = "json", feature = "yaml"))]
-
 use axum::body::Body;
 use relentless::{
     command::{Relentless, ReportFormat},
@@ -10,9 +8,52 @@ use relentless::{
 use relentless_dev_server::route;
 
 #[tokio::test]
-async fn test_example_assault() {
+#[cfg(feature = "json")]
+async fn test_example_yaml_config() {
     let relentless = Relentless {
-        file: glob::glob("tests/config/basic/*").unwrap().collect::<Result<Vec<_>, _>>().unwrap(),
+        file: glob::glob("examples/config/*.yaml").unwrap().collect::<Result<Vec<_>, _>>().unwrap(),
+        report_format: ReportFormat::NullDevice,
+        ..Default::default()
+    };
+    let configs = relentless.configs().unwrap();
+    let services = [
+        ("test-api".to_string(), route::app_with(Default::default())),
+        ("expect".to_string(), route::app_with(Default::default())),
+        ("actual".to_string(), route::app_with(Default::default())),
+    ]
+    .into_iter()
+    .collect();
+    let report = relentless.assault_with::<_, Body, Body, _>(configs, vec![services], &DefaultEvaluator).await.unwrap();
+
+    assert!(report.allow(false));
+}
+
+#[tokio::test]
+#[cfg(all(feature = "json", feature = "yaml"))]
+async fn test_basic_yaml_config() {
+    let relentless = Relentless {
+        file: glob::glob("tests/config/basic/*.yaml").unwrap().collect::<Result<Vec<_>, _>>().unwrap(),
+        report_format: ReportFormat::NullDevice,
+        ..Default::default()
+    };
+    let configs = relentless.configs().unwrap();
+    let services = [
+        ("test-api".to_string(), route::app_with(Default::default())),
+        ("expect".to_string(), route::app_with(Default::default())),
+        ("actual".to_string(), route::app_with(Default::default())),
+    ]
+    .into_iter()
+    .collect();
+    let report = relentless.assault_with::<_, Body, Body, _>(configs, vec![services], &DefaultEvaluator).await.unwrap();
+
+    assert!(report.allow(false));
+}
+
+#[tokio::test]
+#[cfg(all(feature = "json", feature = "toml"))]
+async fn test_basic_toml_config() {
+    let relentless = Relentless {
+        file: glob::glob("tests/config/basic/*.toml").unwrap().collect::<Result<Vec<_>, _>>().unwrap(),
         report_format: ReportFormat::NullDevice,
         ..Default::default()
     };
