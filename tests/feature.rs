f302f3a2cdafc1f9fbd5eadd92fbfc64708070ca
hayas1 <h4ystack@gmail.com>
2024-10-26 17:58:23 +0000

    repeat feature test

---
+++ tests/feature.rs
@@ -0,0 +1,37 @@
+#![cfg(all(feature = "json", feature = "yaml", feature = "console-report"))]
+use axum::body::Body;
+use relentless::{
+    command::{Relentless, ReportFormat},
+    evaluate::DefaultEvaluator,
+    report::{ConsoleReport, ReportWriter, Reportable},
+};
+
+use relentless_dev_server::route;
+
+#[tokio::test]
+async fn test_repeat_config() {
+    let relentless = Relentless {
+        file: vec!["tests/config/feature/repeat.yaml".into()],
+        report_format: ReportFormat::Console,
+        no_color: true,
+        ..Default::default()
+    };
+    let configs = relentless.configs().unwrap();
+    let test_api = route::app_with(Default::default());
+    let services = [("test-api".to_string(), test_api)].into_iter().collect();
+    let report = relentless.assault_with::<_, Body, Body, _>(configs, vec![services], &DefaultEvaluator).await.unwrap();
+
+    let mut buf = Vec::new();
+    report.console_report(&relentless, &mut ReportWriter::new(0, &mut buf)).unwrap();
+    let out = String::from_utf8_lossy(&buf);
+
+    for line in [
+        format!("{} /counter/increment {}10/10", console::Emoji("✅", "PASS"), console::Emoji("🔁", "")),
+        format!("{} /counter/increment/10 {}10/10", console::Emoji("✅", "PASS"), console::Emoji("🔁", "")),
+        format!("{} /counter/decrement {}10/10", console::Emoji("✅", "PASS"), console::Emoji("🔁", "")),
+        format!("{} /counter/decrement/10 {}1/1", console::Emoji("✅", "PASS"), console::Emoji("🔁", "")),
+    ] {
+        assert!(out.contains(&line));
+    }
+    assert!(report.pass());
+}
