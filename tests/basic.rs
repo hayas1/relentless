5c5669b855a5451b65ca5311e6d2cdff36cd60f0
hayas1 <h4ystack@gmail.com>
2024-10-26 09:01:16 +0000

    basics

--- tests/basic.rs
+++ tests/basic.rs
@@ -12,12 +12,18 @@ use relentless_dev_server::route;
 #[tokio::test]
 async fn test_example_assault() {
     let relentless = Relentless {
-        file: vec!["examples/config/assault.yaml".into()],
+        file: glob::glob("tests/config/basic/*").unwrap().collect::<Result<Vec<_>, _>>().unwrap(),
         report_format: ReportFormat::NullDevice,
         ..Default::default()
     };
     let configs = relentless.configs().unwrap();
-    let services = [("test-api".to_string(), route::app_with(Default::default()))].into_iter().collect();
+    let services = [
+        ("test-api".to_string(), route::app_with(Default::default())),
+        ("expect".to_string(), route::app_with(Default::default())),
+        ("actual".to_string(), route::app_with(Default::default())),
+    ]
+    .into_iter()
+    .collect();
     let report = relentless.assault_with::<_, Body, Body, _>(configs, vec![services], &DefaultEvaluator).await.unwrap();
 
     assert!(report.allow(false));
