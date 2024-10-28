734164a8e2b5845f82a85c3a5759e0c479a60842
hayas1 <h4ystack@gmail.com>
2024-10-28 13:37:53 +0000

    allow config test

--- tests/feature.rs
+++ tests/feature.rs
@@ -31,6 +31,7 @@ async fn test_repeat_config() {
         assert!(out.contains(&line));
     }
     assert!(report.pass());
+    assert!(report.allow(false));
 
     let response = services[0]
         .get_mut("test-api")
@@ -56,6 +57,7 @@ async fn test_validate_config() {
 
     assert!(out.contains(&format!("{} /echo/json?foo=hoge&bar=fuga&baz=piyo", CaseConsoleReport::PASS_EMOJI,)));
     assert!(report.pass());
+    assert!(report.allow(false));
 }
 
 #[tokio::test]
@@ -81,4 +83,31 @@ async fn test_fail_validate_config() {
         assert!(out.contains(&line));
     }
     assert!(!report.pass());
+    assert!(!report.allow(false));
+}
+
+#[tokio::test]
+async fn test_allow_config() {
+    let relentless =
+        Relentless { file: vec!["tests/config/feature/allow.yaml".into()], no_color: true, ..Default::default() };
+    let configs = relentless.configs().unwrap();
+    let mut services = vec![[("test-api".to_string(), route::app_with(Default::default()))].into_iter().collect()];
+    let report = relentless.assault_with::<_, Body, Body, _>(configs, &mut services, &DefaultEvaluator).await.unwrap();
+
+    let mut buf = Vec::new();
+    relentless.report_with(&report, &mut buf).unwrap();
+    let out = String::from_utf8_lossy(&buf);
+
+    println!("{}", out);
+
+    for line in [
+        format!("{} /health/disabled", CaseConsoleReport::FAIL_EMOJI),
+        format!("  {} this testcase is allowed", CaseConsoleReport::ALLOW_EMOJI),
+        format!("  {} message was found", CaseConsoleReport::MESSAGE_EMOJI),
+        format!("    status{} is not acceptable", ""),
+    ] {
+        assert!(out.contains(&line));
+    }
+    assert!(!report.pass());
+    assert!(report.allow(false));
 }
