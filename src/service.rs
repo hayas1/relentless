fc4918bc606a81d3feaea07a2aaee81bcc3c91e4
hayas1 <h4ystack@gmail.com>
2024-11-02 17:27:14 +0000

    add test for OriginRouter

--- src/service.rs
+++ src/service.rs
@@ -107,6 +107,62 @@ pub mod origin_router {
             }
         }
     }
+
+    #[cfg(test)]
+    mod tests {
+
+        use http_body_util::{BodyExt, Empty};
+        use relentless_dev_server::route::{self, counter::CounterResponse};
+
+        use super::*;
+
+        #[tokio::test]
+        async fn test_origin_router() {
+            let (service1, service2) = (route::app_with(Default::default()), route::app_with(Default::default()));
+            let mut service = OriginRouter::new(
+                [
+                    (Authority::from_static("localhost:3000"), service1),
+                    (Authority::from_static("localhost:3001"), service2),
+                ]
+                .into_iter()
+                .collect(),
+            );
+            let request1 =
+                http::Request::builder().uri("http://localhost:3000/counter/increment").body(Empty::new()).unwrap();
+            let response1 = service.call(request1).await.unwrap();
+            assert_eq!(response1.status(), 200);
+            let bytes1 =
+                BodyExt::collect(response1.into_body()).await.map(http_body_util::Collected::to_bytes).unwrap();
+            let count1: CounterResponse<i64> = serde_json::from_slice(&bytes1).unwrap();
+            assert_eq!(count1, CounterResponse { count: 1 });
+
+            let request2 = http::Request::builder().uri("http://localhost:3001/counter").body(Empty::new()).unwrap();
+            let response2 = service.call(request2).await.unwrap();
+            assert_eq!(response2.status(), 200);
+            let bytes2 =
+                BodyExt::collect(response2.into_body()).await.map(http_body_util::Collected::to_bytes).unwrap();
+            let count2: CounterResponse<i64> = serde_json::from_slice(&bytes2).unwrap();
+            assert_eq!(count2, CounterResponse { count: 0 });
+        }
+
+        #[tokio::test]
+        async fn test_origin_router_not_found() {
+            let mut service = OriginRouter::new(
+                [(Authority::from_static("localhost:3000"), route::app(Default::default()))].into_iter().collect(),
+            );
+            let request = http::Request::builder().uri("http://localhost:3000").body(Empty::new()).unwrap();
+            let response = service.call(request).await.unwrap();
+            assert_eq!(response.status(), 200);
+            assert_eq!(
+                &BodyExt::collect(response.into_body()).await.map(http_body_util::Collected::to_bytes).unwrap()[..],
+                b"Hello World"
+            );
+
+            let request = http::Request::builder().uri("http://localhost:8000").body(Empty::new()).unwrap();
+            let err = service.call(request).await.unwrap_err();
+            assert!(matches!(err.downcast_ref(), Some(AssaultError::CannotSpecifyService)));
+        }
+    }
 }
 
 pub struct BytesBody(BoxBody<Bytes, crate::Error>);
