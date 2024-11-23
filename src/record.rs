bd0c4e7a7ff64c9e19737b21f14b202d15038dda
hayas1 <h4ystack@gmail.com>
2024-11-23 15:31:33 +0000

    record for response

--- src/record.rs
+++ src/record.rs
@@ -28,12 +28,40 @@ where
 {
     type Error = std::io::Error;
     async fn record<W: std::io::Write>(&self, _w: &mut W) -> Result<(), Self::Error> {
-        // TODO
+        // TODO from content-type
         unimplemented!("json");
     }
     async fn record_raw<W: std::io::Write>(&self, w: &mut W) -> Result<(), Self::Error> {
         let (method, uri, version) = (self.method(), self.uri(), self.version());
         writeln!(w, "{} {} {:?}", method, uri, version)?;
+
+        let headers = self.headers();
+        for (header, value) in headers.iter() {
+            writeln!(w, "{}: {:?}", header, value)?;
+        }
+        writeln!(w)?;
+
+        let body = self.body().clone();
+        if let Ok(b) = BodyExt::collect(body).await.map(Collected::to_bytes) {
+            write!(w, "{}", String::from_utf8_lossy(&b))?;
+        }
+
+        Ok(())
+    }
+}
+
+impl<B> Recordable for http::Response<B>
+where
+    B: Body + Clone,
+{
+    type Error = std::io::Error;
+    async fn record<W: std::io::Write>(&self, _w: &mut W) -> Result<(), Self::Error> {
+        // TODO from content-type
+        unimplemented!("json");
+    }
+    async fn record_raw<W: std::io::Write>(&self, w: &mut W) -> Result<(), Self::Error> {
+        let (version, status) = (self.version(), self.status());
+        writeln!(w, "{:?} {}", version, status)?;
 
         let headers = self.headers();
         for (header, value) in headers.iter() {
@@ -68,5 +96,15 @@ mod tests {
         let mut buf = Vec::new();
         request.record_raw(&mut buf).await.unwrap();
         assert_eq!(buf, b"GET http://localhost:3000/ HTTP/1.1\n\n");
+    }
+
+    #[tokio::test]
+    async fn test_empty_body_response() {
+        let response =
+            http::Response::builder().status(http::StatusCode::OK).body(http_body_util::Empty::<Bytes>::new()).unwrap();
+
+        let mut buf = Vec::new();
+        response.record_raw(&mut buf).await.unwrap();
+        assert_eq!(buf, b"HTTP/1.1 200 OK\n\n");
     }
 }
