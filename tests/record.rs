7af658c69b1b0a8cf1fc278cc965f70c3be6488c
hayas1 <h4ystack@gmail.com>
2024-11-29 13:42:37 +0000

    add test

--- tests/record.rs
+++ tests/record.rs
@@ -23,10 +23,10 @@ async fn test_record_config() {
     let gitignore_content = std::fs::read_to_string(gitignore_path).unwrap();
     assert_eq!(gitignore_content, "*\n");
 
-    let request_path = "tests/record_test_directory/http:/localhost:3000/echo/body/request.txt";
-    let request_record = std::fs::read_to_string(request_path).unwrap();
+    let raw_request_path = "tests/record_test_directory/http:/localhost:3000/echo/body/raw_request.txt";
+    let raw_request_record = std::fs::read_to_string(raw_request_path).unwrap();
     assert_eq!(
-        request_record,
+        raw_request_record,
         indoc::indoc! {
           r#"POST http://localhost:3000/echo/body HTTP/1.1
             content-type: "text/plain"
@@ -36,10 +36,10 @@ async fn test_record_config() {
         }
     );
 
-    let response_path = "tests/record_test_directory/http:/localhost:3000/echo/body/response.txt";
-    let response_record = std::fs::read_to_string(response_path).unwrap();
+    let raw_response_path = "tests/record_test_directory/http:/localhost:3000/echo/body/raw_response.txt";
+    let raw_response_record = std::fs::read_to_string(raw_response_path).unwrap();
     assert_eq!(
-        response_record,
+        raw_response_record,
         indoc::indoc! {
           r#"HTTP/1.1 200 OK
             content-type: "application/octet-stream"
@@ -48,4 +48,12 @@ async fn test_record_config() {
             hello world"#
         }
     );
+
+    let request_path = "tests/record_test_directory/http:/localhost:3000/health/rich/request.txt";
+    let request_record = std::fs::read_to_string(request_path).unwrap();
+    assert_eq!(request_record, "");
+
+    let response_path = "tests/record_test_directory/http:/localhost:3000/health/rich/response.json";
+    let response_record = std::fs::read_to_string(response_path).unwrap();
+    assert_eq!(response_record, r#"{"status":"200 OK","code":200}"#);
 }
