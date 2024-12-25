ebc8ec2c50f601cd19f5c88468798b3d852dac52
hayas1 <h4ystack@gmail.com>
2024-12-25 13:39:08 +0000

    add test for allow override (as setting)

--- tests/feature.rs
+++ tests/feature.rs
@@ -117,6 +117,40 @@ async fn test_allow_config() {
 }
 
 #[tokio::test]
+async fn test_allow_override_config() {
+    let relentless = Relentless {
+        file: vec!["tests/config/feature/allow_override.yaml".into()],
+        no_color: true,
+        ..Default::default()
+    };
+    let configs = relentless.configs().unwrap();
+    let service = route::app_with(Default::default());
+    let report = relentless.assault_with::<_, Request<Body>>(configs, service).await.unwrap();
+
+    let mut buf = Vec::new();
+    relentless.report_with(&report, &mut buf).unwrap();
+    let out = String::from_utf8_lossy(&buf);
+
+    for line in [
+        format!("{} /health/disabled", CaseConsoleReport::FAIL_EMOJI),
+        format!("  {} this testcase is allowed", CaseConsoleReport::ALLOW_EMOJI),
+        format!("  {} message was found", CaseConsoleReport::MESSAGE_EMOJI),
+        format!("    status{} is not acceptable", ""),
+        format!("{} /health/disabled", CaseConsoleReport::FAIL_EMOJI),
+        format!("  {} this testcase is allowed", CaseConsoleReport::ALLOW_EMOJI),
+        format!("  {} message was found", CaseConsoleReport::MESSAGE_EMOJI),
+        format!("    status{} is not acceptable", ""),
+        format!("{} /health/disabled", CaseConsoleReport::FAIL_EMOJI),
+        format!("  {} message was found", CaseConsoleReport::MESSAGE_EMOJI),
+        format!("    status{} is not acceptable", ""),
+    ] {
+        assert!(out.contains(&line));
+    }
+    assert!(!relentless.pass(&report));
+    assert!(!relentless.allow(&report));
+}
+
+#[tokio::test]
 async fn test_json_diff_config() {
     let relentless =
         Relentless { file: vec!["tests/config/feature/json_diff.yaml".into()], no_color: true, ..Default::default() };
