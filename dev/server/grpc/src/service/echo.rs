1eadc2a1587f4a2428a054381032173a0a042e03
hayas1 <h4ystack@gmail.com>
2025-01-01 12:53:23 +0000

    add test

--- dev/server/grpc/src/service/echo.rs
+++ dev/server/grpc/src/service/echo.rs
@@ -21,3 +21,28 @@ impl pb::echo_server::Echo for EchoImpl {
         Ok(Response::new(value))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use pb::echo_server::Echo;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_echo() {
+        let echo = EchoImpl;
+
+        let request = Request::new(Any::from_msg(&"100".to_string()).unwrap());
+        let response = echo.echo(request).await.unwrap();
+        assert_eq!(response.into_inner().to_msg::<String>().unwrap(), "100");
+    }
+
+    #[tokio::test]
+    async fn test_echo_value() {
+        let echo = EchoImpl;
+
+        let request = Request::new(Value::from(200));
+        let response = echo.echo_value(request).await.unwrap();
+        assert_eq!(response.into_inner(), Value::from(200));
+    }
+}
