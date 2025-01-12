76c5c9e52a624a83c44c8e1efaeecdf9ec749d31
hayas1 <h4ystack@gmail.com>
2025-01-12 07:42:58 +0000

    clang-format in CI

--- dev/server/grpc/scripts/setup.sh
+++ dev/server/grpc/scripts/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt-get install -y protobuf-compiler
-# sudo apt-get install -y clang-format
+sudo apt-get install -y protobuf-compiler clang-format
 
 go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
