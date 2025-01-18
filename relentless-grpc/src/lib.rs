443b07ece54c90a0337d3190de6960c972134489
hayas1 <h4ystack@gmail.com>
2025-01-18 04:46:54 +0000

    http / grpc document

--- relentless-grpc/src/lib.rs
+++ relentless-grpc/src/lib.rs
@@ -1,7 +1,60 @@
-//! Relentless GRPC comparison testing tool
+//! GRPC implementation of comparison testing framework [relentless].
 //!
-//! # Usage
-//! TODO: see [relentless]
+//! # Binary Usage
+//! About the same as [relentless]
+//! | step | command |
+//! | --- | --- |
+//! | install binary | `cargo install --git https://github.com/hayas1/relentless relentless-grpc --features cli` |
+//! | install dev server | `cargo install --git https://github.com/hayas1/relentless relentless-grpc-dev-server` |
+//! | run command | `relentless-grpc -f compare.yaml` |
+//!
+//! # Library Usage
+//! ## Install
+//! Often used in dev-dependencies.
+//! ```sh
+//! cargo add --dev --git https://github.com/hayas1/relentless relentless-grpc
+//! ```
+//! ```toml
+//! [dev-dependencies]
+//! relentless-grpc = { git = "https://github.com/hayas1/relentless" }
+//! ```
+//!
+//! ## Testing
+//! ```
+//! # tokio_test::block_on(async {
+//! use relentless::interface::{
+//!     command::{Assault, Relentless},
+//!     config::{Config, Format},
+//! };
+//! use relentless_grpc::{client::DefaultGrpcClient, command::GrpcAssault};
+//! use relentless_grpc_dev_server::service;
+//!
+//! let assault = GrpcAssault::new(Relentless {
+//!     file: vec![], // files can be specified also
+//!     ..Default::default()
+//! });
+//! let config = r#"
+//!   name: basic grpc comparison test
+//!   destinations:
+//!       actual: http://localhost:50051
+//!       expect: http://localhost:50051
+//!
+//!   testcases:
+//!   - target: greeter.Greeter/SayHello
+//!     setting:
+//!       request:
+//!         message:
+//!           json:
+//!             name: John Doe
+//! "#;
+//!
+//! // TODO let configs = vec![Config::read_str(config, Format::Yaml).unwrap()];
+//! // TODO let service = service::app(Default::default());
+//! // TODO let report = assault.assault_with(configs, service).await.unwrap();
+//!
+//! // TODO assert!(assault.pass(&report));
+//! # })
+//! ```
 
 pub mod client;
 pub mod command;
--- relentless-http/src/lib.rs
+++ relentless-http/src/lib.rs
@@ -1,7 +1,58 @@
-//! Relentless HTTP comparison testing tool
+//! HTTP implementation of comparison testing framework [relentless].
 //!
-//! # Usage
-//! TODO: see [relentless]
+//! # Binary Usage
+//! About the same as [relentless]
+//! | step | command |
+//! | --- | --- |
+//! | install binary | `cargo install --git https://github.com/hayas1/relentless relentless-http --features cli` |
+//! | install dev server | `cargo install --git https://github.com/hayas1/relentless relentless-http-dev-server` |
+//! | run command | `relentless-http -f compare.yaml` |
+//!
+//! # Library Usage
+//! ## Install
+//! Often used in dev-dependencies.
+//! ```sh
+//! cargo add --dev --git https://github.com/hayas1/relentless relentless-http
+//! ```
+//! ```toml
+//! [dev-dependencies]
+//! relentless-http = { git = "https://github.com/hayas1/relentless" }
+//! ```
+//!
+//! ## Testing
+//! ```
+//! # tokio_test::block_on(async {
+//! use axum::body::Body;
+//! use relentless::interface::{
+//!     command::{Assault, Relentless},
+//!     config::{Config, Format},
+//! };
+//! use relentless_http::command::HttpAssault;
+//! use relentless_http_dev_server::route;
+//!
+//! let assault = HttpAssault::<Body, Body>::new(Relentless {
+//!     file: vec![], // files can be specified also
+//!     ..Default::default()
+//! });
+//! let config = r#"
+//!     name: basic http comparison test
+//!     destinations:
+//!         actual: http://localhost:3000
+//!         expect: http://localhost:3000
+//!
+//!     testcases:
+//!     - target: /
+//!     - target: /health
+//!     - target: /healthz
+//! "#;
+//!
+//! let configs = vec![Config::read_str(config, Format::Yaml).unwrap()];
+//! let service = route::app_with(Default::default());
+//! let report = assault.assault_with(configs, service).await.unwrap();
+//!
+//! assert!(assault.pass(&report));
+//! # })
+//! ```
 
 #[cfg(feature = "default-http-client")]
 pub mod client;
