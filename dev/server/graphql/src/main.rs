cc9b390359c5715d4addf55defe07bd8c6d5d5ea
hayas1 <h4ystack@gmail.com>
2025-02-03 13:39:18 +0000

    run the example of book

--- dev/server/graphql/src/main.rs
+++ dev/server/graphql/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use async_graphql::{http::GraphiQLSource, Schema};
+use async_graphql_axum::{GraphQL, GraphQLSubscription};
+use axum::{
+    response::{self, IntoResponse},
+    routing::get,
+    Router,
+};
+use relentless_graphql_dev_server::{MutationRoot, QueryRoot, Storage, SubscriptionRoot};
+use tokio::net::TcpListener;
+
+async fn graphiql() -> impl IntoResponse {
+    response::Html(GraphiQLSource::build().endpoint("/").subscription_endpoint("/ws").finish())
+}
+
+#[tokio::main]
+async fn main() {
+    let schema = Schema::build(QueryRoot, MutationRoot, SubscriptionRoot).data(Storage::default()).finish();
+
+    let app = Router::new()
+        .route("/", get(graphiql).post_service(GraphQL::new(schema.clone())))
+        .route_service("/ws", GraphQLSubscription::new(schema));
+
+    println!("GraphiQL IDE: http://localhost:8000");
+
+    axum::serve(TcpListener::bind("127.0.0.1:8000").await.unwrap(), app).await.unwrap();
 }
