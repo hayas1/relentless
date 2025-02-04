0bd86d4509251fbf3351ef4a01d8531c7541f150
hayas1 <h4ystack@gmail.com>
2025-02-04 15:05:59 +0000

    tracing

--- dev/server/graphql/src/lib.rs
+++ dev/server/graphql/src/lib.rs
@@ -5,16 +5,16 @@ pub mod simple_broker;
 pub mod state;
 
 pub async fn serve(env: env::Env) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    //     tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt::init();
     let listener = tokio::net::TcpListener::bind(&env.bind()).await?;
     let app = route::app(env);
 
-    //     tracing::info!("start app on {}", listener.local_addr()?);
+    tracing::info!("start app on {}", listener.local_addr()?);
     axum::serve(listener, app)
         .with_graceful_shutdown(async {
             tokio::signal::ctrl_c().await.expect("failed to install Ctrl+C handler");
         })
         .await?;
-    //     tracing::info!("stop app");
+    tracing::info!("stop app");
     Ok(())
 }
