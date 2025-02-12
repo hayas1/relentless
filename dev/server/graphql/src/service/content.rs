a06f8c530b4687c5ee4abebbf60d7d0367f4f3c0
hayas1 <h4ystack@gmail.com>
2025-02-12 15:17:23 +0000

    test for create

--- dev/server/graphql/src/service/content.rs
+++ dev/server/graphql/src/service/content.rs
@@ -75,4 +75,40 @@ mod tests {
         let res = schema.execute(query).await.data;
         assert_eq!(res, value!({"content": {"id": "0", "name": "test" }}));
     }
+
+    #[tokio::test]
+    async fn test_create_content() {
+        let contents: Arc<Mutex<Slab<Content>>> = Default::default();
+        let state = AppState { contents, ..Default::default() };
+        let schema = Schema::build(QueryRoot, MutationRoot, SubscriptionRoot).data(state).finish();
+
+        let mutation0 = r#"
+            mutation {
+              createContent(name :"test0")
+            }
+        "#;
+        let mutation1 = r#"
+            mutation {
+              createContent(name :"test1")
+            }
+        "#;
+        let mutation2 = r#"
+            mutation {
+              createContent(name :"test2")
+            }
+        "#;
+        schema.execute(mutation0).await;
+        schema.execute(mutation1).await;
+        schema.execute(mutation2).await;
+
+        let query = r#"
+            query {
+                contents {
+                    id
+                }
+            }
+        "#;
+        let res = schema.execute(query).await.data;
+        assert_eq!(res, value!({"contents": [{"id": "0"}, {"id": "1"}, {"id": "2"}]}));
+    }
 }
