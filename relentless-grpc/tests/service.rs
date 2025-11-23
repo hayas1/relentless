e6130393d4b6ff76a22e029ffc3c63590af6d682
hayas1 <h4ystack@gmail.com>
2025-11-23 12:19:00 +0000

    wip: test grpc server

---
+++ relentless-grpc/tests/service.rs
@@ -0,0 +1,19 @@
+use relentless::{
+    report::ReportFormat,
+    shot::job::{Job, JobSpec},
+};
+use relentless_grpc::{request::GrpcRequest, response::GrpcResponse, service::DynamicContract};
+use relentless_grpc_dev_server::service::greeter::{pb::greeter_server::GreeterServer, GreeterImpl};
+use tower::make::Shared;
+
+#[tokio::test]
+async fn test_example_yaml_config() {
+    let spec = JobSpec { report_format: ReportFormat::NullDevice, ..Default::default() };
+    let files: Result<Vec<_>, _> = glob::glob("examples/config/*.yaml").unwrap().collect();
+    let job = Job::<GrpcRequest, GrpcResponse>::from_files(&files.unwrap()).unwrap();
+
+    let server = Shared::new(GreeterServer::new(GreeterImpl));
+    let report = job.shot::<_, _, DynamicContract<serde_json::Value, _>>(server, &spec).await.unwrap();
+
+    assert!(report.pass());
+}
