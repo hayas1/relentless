b15695a05d1727120ed8cb1638b865cd3a03f409
hayas1 <h4ystack@gmail.com>
2025-12-04 16:43:46 +0000

    agg method

--- relentless/src/record/metric.rs
+++ relentless/src/record/metric.rs
@@ -3,9 +3,10 @@ use std::{
     pin::Pin,
     sync::{Arc, Mutex},
     task::{Context, Poll},
-    time::{Instant, SystemTime},
+    time::{Duration, Instant, SystemTime},
 };
 
+use bytesize::ByteSize;
 use pin_project::pin_project;
 use semigroup::{op::HdrHistogram, Monoid, OptionMonoid, Semigroup};
 use tower::{Layer, Service};
@@ -26,7 +27,7 @@ impl Metric {
 }
 
 #[derive(Debug, Clone, PartialEq, Semigroup)]
-pub struct MetricAgg {
+struct MetricAggInner {
     #[semigroup(with = "semigroup::op::Sum")]
     times: u64,
     #[semigroup(with = "semigroup::op::Min")]
@@ -36,22 +37,50 @@ pub struct MetricAgg {
     bytes: HdrHistogram<u64>,
     latency: HdrHistogram<u64>,
 }
+#[derive(Debug, Clone, PartialEq, Semigroup)]
+#[semigroup(monoid)]
+pub struct MetricAgg(OptionMonoid<MetricAggInner>);
 impl From<Metric> for MetricAgg {
     fn from(value: Metric) -> Self {
         let (start, end) = value.duration;
         let bytes = value.bytes.into();
         let latency = ((end - start).as_millis() as u64).into();
-        MetricAgg { times: 1, start, end, bytes, latency }
+        Self(MetricAggInner { times: 1, start, end, bytes, latency }.into())
+    }
+}
+impl MetricAgg {
+    pub fn times(&self) -> u64 {
+        self.0.as_ref().map(|agg| agg.times).unwrap_or(0)
+    }
+    pub fn duration(&self) -> Duration {
+        self.0.as_ref().map(|agg| agg.end - agg.start).unwrap_or_default()
+    }
+    pub fn rps(&self) -> f64 {
+        self.times() as f64 / self.duration().as_secs_f64()
+    }
+    pub fn approx_bytes_quantile(&self, quantile: f64) -> ByteSize {
+        self.0
+            .as_ref()
+            .map(|agg| agg.bytes.histogram().value_at_quantile(quantile))
+            .map(ByteSize::b)
+            .unwrap_or_default()
+    }
+    pub fn approx_latency_quantile(&self, quantile: f64) -> Duration {
+        self.0
+            .as_ref()
+            .map(|agg| agg.latency.histogram().value_at_quantile(quantile))
+            .map(Duration::from_millis)
+            .unwrap_or_default()
     }
 }
 
 #[derive(Debug, Clone)]
 pub struct MeasureLayer {
-    agg: Arc<Mutex<OptionMonoid<MetricAgg>>>,
+    agg: Arc<Mutex<MetricAgg>>,
 }
 impl MeasureLayer {
     pub fn new() -> Self {
-        let agg = Arc::new(Mutex::new(OptionMonoid::identity()));
+        let agg = Arc::new(Mutex::new(MetricAgg::identity()));
         Self { agg }
     }
 }
@@ -67,7 +96,7 @@ impl<S> Layer<S> for MeasureLayer {
 #[derive(Debug, Clone)]
 pub struct MeasureService<S> {
     inner: S,
-    agg: Arc<Mutex<OptionMonoid<MetricAgg>>>,
+    agg: Arc<Mutex<MetricAgg>>,
 }
 
 impl<S, Req> Service<Req> for MeasureService<S>
@@ -93,10 +122,10 @@ pub struct MeasureFuture<F> {
     fut: F,
     start: Option<(SystemTime, Instant)>,
     end: Option<Box<dyn FnOnce() -> Instant>>,
-    agg: Arc<Mutex<OptionMonoid<MetricAgg>>>,
+    agg: Arc<Mutex<MetricAgg>>,
 }
 impl<F> MeasureFuture<F> {
-    pub fn new(fut: F, agg: Arc<Mutex<OptionMonoid<MetricAgg>>>) -> Self {
+    pub fn new(fut: F, agg: Arc<Mutex<MetricAgg>>) -> Self {
         Self { fut, start: None, end: Some(Box::new(Instant::now)), agg }
     }
 }
@@ -118,8 +147,8 @@ where
             let end = this.end.take().expect("poll after ready")();
 
             let metric = Metric::new(0, timestamp, (start, end)).into_agg();
-            let mut owned = this.agg.lock().unwrap();
-            owned.semigroup_assign(metric.into());
+            let mut agg = this.agg.lock().unwrap();
+            agg.semigroup_assign(metric);
 
             o
         })
@@ -128,7 +157,7 @@ where
 
 #[cfg(test)]
 mod tests {
-    use std::{convert::Infallible, time::Duration};
+    use std::convert::Infallible;
 
     use futures::StreamExt;
     use tower::{ServiceBuilder, ServiceExt};
@@ -150,9 +179,10 @@ mod tests {
 
         let count = stream.count().await;
 
-        let g = measure.agg.lock().unwrap();
-        let metric = g.as_ref().unwrap();
-        assert_eq!(metric.times, count as u64);
-        assert!((1000..1100).contains(&metric.latency.histogram().value_at_percentile(99.)))
+        // TODO runtime
+        let agg = measure.agg.lock().unwrap();
+        assert_eq!(agg.times(), count as u64);
+        assert!((175. ..180.).contains(&agg.rps()));
+        assert!((Duration::from_millis(1000)..Duration::from_millis(1100)).contains(&agg.approx_latency_quantile(0.99)))
     }
 }
