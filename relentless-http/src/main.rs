ca2f6ad682870ce3d5c3f4e9d169d7108f11becd
hayas1 <h4ystack@gmail.com>
2025-12-15 10:51:57 +0000

    Cli::run in http

--- relentless-http/src/main.rs
+++ relentless-http/src/main.rs
@@ -3,19 +3,25 @@ use std::process::ExitCode;
 #[cfg(feature = "cli")]
 #[tokio::main]
 pub async fn main() -> Result<ExitCode, Box<dyn std::error::Error>> {
-    use relentless::{record::metric::MeasureLayer, report::Reporter, shot::job::Cli};
+    use relentless::{
+        record::metric::MeasureLayer,
+        report::Reporter,
+        shot::job::{Cli, Job},
+    };
     use relentless_http::{contract::HttpContract, service::ReqwestClient};
     use reqwest::Body;
     use tower::ServiceBuilder;
 
-    let (job, spec) = Cli::job().await?;
-    let measure = MeasureLayer::new();
-    let client = ReqwestClient::new().await?;
-    let service = ServiceBuilder::new().layer(&measure).service(client);
-    let report = job.shot::<_, _, HttpContract<Body, Body>>(tower::make::Shared::new(service), &spec).await?;
-    spec.report_format.report(&report)?;
-    dbg!(measure.aggregated().times());
-    Ok((!report.evaluated.pass as u8).into())
+    Cli::run(|job: Job<_, _, _>, spec| async move {
+        let measure = MeasureLayer::new();
+        let client = ReqwestClient::new().await?;
+        let service = ServiceBuilder::new().layer(&measure).service(client);
+        let report = job.shot::<_, _, HttpContract<Body, Body>>(tower::make::Shared::new(service), &spec).await?;
+        spec.report_format.report(&report)?;
+        dbg!(measure.aggregated().times());
+        Ok((!report.evaluated.pass as u8).into())
+    })
+    .await
 }
 
 #[cfg(not(feature = "cli"))]
