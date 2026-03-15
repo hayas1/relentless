016b2f3c15736a6d4d80adbbf8cdbe19667dcaf4
hayas1 <h4ystack@gmail.com>
2026-03-15 06:37:43 +0000

    modify test

--- relentless-grpc/tests/service.rs
+++ relentless-grpc/tests/service.rs
@@ -3,16 +3,20 @@ use relentless::{
     shot::job::{Job, JobSpec},
 };
 use relentless_grpc::{contract::DynamicContract, wip::JsonSerializer};
-use relentless_grpc_dev_server::app::greeter::{pb::greeter_server::GreeterServer, GreeterImpl};
+use relentless_grpc_dev_server::runner::RunCommand;
 use tower::make::Shared;
 
 #[tokio::test]
 async fn test_example_yaml_config() {
-    let spec = JobSpec { report_format: ReportFormat::NullDevice, ..Default::default() };
+    let spec = JobSpec {
+        report_format: ReportFormat::NullDevice,
+        base_path: Some("..".parse().unwrap()),
+        ..Default::default()
+    };
     let files: Result<Vec<_>, _> = glob::glob("examples/config/*.yaml").unwrap().collect();
     let job = Job::from_files(&files.unwrap()).unwrap();
 
-    let server = Shared::new(GreeterServer::new(GreeterImpl));
+    let server = Shared::new(RunCommand::default().app().routes());
     let report = job.shot::<_, _, DynamicContract<serde_json::Value, JsonSerializer>>(server, &spec).await.unwrap();
 
     assert!(report.evaluated.allow);
