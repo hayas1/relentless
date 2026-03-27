3222efb5a32d26cc5c881c0cd518bc6b70eb2c9c
hayas1 <h4ystack@gmail.com>
2026-03-27 08:20:44 +0000

    value

--- relentless/src/testing.rs
+++ relentless/src/testing.rs
@@ -9,6 +9,7 @@ use std::{
 use futures::{StreamExt, TryStreamExt};
 use semigroup::Semigroup;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use tower::{layer::util::Identity, Layer, Service};
 
 use crate::{
@@ -25,19 +26,22 @@ use crate::{
 };
 
 #[derive(Debug, Clone, PartialEq, Eq, Default, Serialize, Deserialize, Semigroup)]
-pub struct StringRequest {
+pub struct ValueRequest {
     #[semigroup(with = "semigroup::op::Coalesce")]
-    pub message: Option<String>,
+    pub arg: Option<Value>,
 }
-impl RequestSource<String> for StringRequest {
+impl RequestSource<Value> for ValueRequest {
     type Error = Infallible;
-    async fn produce(&self, _: &http::Uri, _: &str) -> Result<String, Self::Error> {
-        Ok(self.message.clone().unwrap_or_default())
+    async fn produce(&self, _: &http::Uri, target: &str) -> Result<Value, Self::Error> {
+        match target {
+            "echo" => Ok(self.arg.clone().unwrap_or_default()),
+            _ => todo!(),
+        }
     }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Default, Serialize, Deserialize, Semigroup)]
-pub struct StringResponse {
+pub struct ValueResponse {
     #[semigroup(with = "semigroup::op::Coalesce")]
     pub message: Option<StringResponseInner>,
 }
@@ -45,15 +49,15 @@ pub struct StringResponse {
 pub enum StringResponseInner {
     #[default]
     AnyOrEqual,
-    Expect(ExpectEvaluator<String>),
+    Expect(ExpectEvaluator<Value>),
     // Regex(RegexEvaluator), // TODO sized
 }
-impl<E: Display + Send> ResponseSink<Result<String, E>> for StringResponse {
+impl<E: Display + Send> ResponseSink<Result<Value, E>> for ValueResponse {
     type Message = EvaluateError;
     async fn consume(
         &self,
         msg: &mut Messages<Self::Message>,
-        res: Destinations<Result<String, E>>,
+        res: Destinations<Result<Value, E>>,
     ) -> Result<(), Failure> {
         let buffers = res.len().max(1);
         let collected: Destinations<_> = futures::stream::iter(res)
@@ -65,15 +69,15 @@ impl<E: Display + Send> ResponseSink<Result<String, E>> for StringResponse {
         self.message.as_ref().unwrap_or(&Default::default()).evaluate(msg, collected)
     }
 }
-impl Evaluator<String> for StringResponseInner {
+impl Evaluator<Value> for StringResponseInner {
     type Message = EvaluateError;
-    fn evaluate_shot(&self, msg: &mut Messages<Self::Message>, res: &String) -> Result<(), Failure> {
+    fn evaluate_shot(&self, msg: &mut Messages<Self::Message>, res: &Value) -> Result<(), Failure> {
         match self {
             Self::AnyOrEqual => Ok(()),
             Self::Expect(e) => e.evaluate_shot(msg, res),
         }
     }
-    fn evaluate_compare(&self, msg: &mut Messages<Self::Message>, res1: &String, res2: &String) -> Result<(), Failure> {
+    fn evaluate_compare(&self, msg: &mut Messages<Self::Message>, res1: &Value, res2: &Value) -> Result<(), Failure> {
         match self {
             Self::AnyOrEqual => self.evaluate_bool(msg, res1 == res2, |_| EvaluateError::custom("not equal body")),
             Self::Expect(e) => e.evaluate_compare(msg, res1, res2),
@@ -82,20 +86,20 @@ impl Evaluator<String> for StringResponseInner {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Default, Serialize, Deserialize, Semigroup)]
-pub struct EchoClient;
-impl Service<String> for EchoClient {
-    type Response = String;
+pub struct TestingClient;
+impl Service<Value> for TestingClient {
+    type Response = Value;
     type Error = Infallible;
     type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
     fn poll_ready(&mut self, _: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         Poll::Ready(Ok(()))
     }
-    fn call(&mut self, req: String) -> Self::Future {
+    fn call(&mut self, req: Value) -> Self::Future {
         Box::pin(async move { Ok(req) })
     }
 }
 // MakeService
-impl Service<http::Uri> for EchoClient {
+impl Service<http::Uri> for TestingClient {
     type Response = Self;
     type Error = Infallible;
     type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
@@ -106,28 +110,28 @@ impl Service<http::Uri> for EchoClient {
         Box::pin(async move { Ok(Self) })
     }
 }
-impl<S> Layer<S> for EchoClient {
+impl<S> Layer<S> for TestingClient {
     type Service = <Identity as Layer<S>>::Service;
 
     fn layer(&self, service: S) -> Self::Service {
         Identity::new().layer(service)
     }
 }
-impl Contract<Self> for EchoClient {
+impl Contract<Self> for TestingClient {
     type Sign = Self;
-    type ReqSource = StringRequest;
-    type Request = String;
-    type TransportReq = String;
-    type TransportRes = String;
-    type Response = String;
-    type ResSink = StringResponse;
+    type ReqSource = ValueRequest;
+    type Request = Value;
+    type TransportReq = Value;
+    type TransportRes = Value;
+    type Response = Value;
+    type ResSink = ValueResponse;
 
     type SignError = Infallible;
 }
-impl SignContract<Self, Self> for EchoClient {
+impl SignContract<Self, Self> for TestingClient {
     type Error = Infallible;
     async fn sign_contract(&self, _: Self, _: &http::Uri, _: &Option<BasePath>) -> Result<Self, Self::Error> {
-        Ok(EchoClient)
+        Ok(TestingClient)
     }
 }
 
@@ -148,9 +152,9 @@ mod tests {
     #[tokio::test]
     async fn test_compile_with_echo_service() {
         let (job, spec) = (Job(Vec::new()), JobSpec::default());
-        let make = EchoClient;
+        let make = TestingClient;
 
-        let report = job.shot::<EchoClient, EchoClient, EchoClient>(make, &spec).await.unwrap();
+        let report = job.shot::<TestingClient, TestingClient, TestingClient>(make, &spec).await.unwrap();
         spec.report(&report).unwrap();
         assert!(report.evaluated.assess().success());
     }
@@ -160,7 +164,7 @@ mod tests {
         let suites = vec![SuiteCase {
             suite: Suite {
                 name: "echo".to_string(),
-                contract: Some(EchoClient),
+                contract: Some(TestingClient),
                 destinations: vec![("test", crate::http_newtype_serde::Uri("http://localhost:8080".parse().unwrap()))]
                     .into_iter()
                     .collect(),
@@ -168,22 +172,26 @@ mod tests {
             },
             testcases: vec![
                 Testcase {
-                    target: "echo test case 1: hello".to_string(),
+                    target: "echo".to_string(),
                     profile: Profile {
-                        request: StringRequest { message: Some("hello".to_string()) },
-                        response: StringResponse {
-                            message: Some(StringResponseInner::Expect(ExpectEvaluator::new("hello".to_string()))),
+                        request: ValueRequest { arg: Some(serde_json::json!("hello")) },
+                        response: ValueResponse {
+                            message: Some(StringResponseInner::Expect(ExpectEvaluator::new(serde_json::json!(
+                                "hello"
+                            )))),
                         },
                         ..Default::default()
                     },
                     ..Default::default()
                 },
                 Testcase {
-                    target: "echo test case 2: world".to_string(),
+                    target: "echo".to_string(),
                     profile: Profile {
-                        request: StringRequest { message: Some("world".to_string()) },
-                        response: StringResponse {
-                            message: Some(StringResponseInner::Expect(ExpectEvaluator::new("world".to_string()))),
+                        request: ValueRequest { arg: Some(serde_json::json!("value")) },
+                        response: ValueResponse {
+                            message: Some(StringResponseInner::Expect(ExpectEvaluator::new(serde_json::json!(
+                                "value"
+                            )))),
                         },
                         ..Default::default()
                     },
@@ -192,9 +200,9 @@ mod tests {
             ],
         }];
         let (job, spec) = (Job(suites), JobSpec { report_format: ReportFormat::NullDevice, ..Default::default() });
-        let make = EchoClient;
+        let make = TestingClient;
 
-        let report = job.shot::<EchoClient, EchoClient, EchoClient>(make, &spec).await.unwrap();
+        let report = job.shot::<TestingClient, TestingClient, TestingClient>(make, &spec).await.unwrap();
         spec.report(&report).unwrap();
         assert!(report.evaluated.assess().success());
     }
